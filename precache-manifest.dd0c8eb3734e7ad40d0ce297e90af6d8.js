2a05d9385eb662ec666ca10c59043d943e04696f
csbabcock <52185272+csbabcock@users.noreply.github.com>
2021-05-20 10:00:10 -0400

    Updates

---
+++ precache-manifest.dd0c8eb3734e7ad40d0ce297e90af6d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02843e8a8dcd2bb2e94f3f65aae08bdf",
+    "url": "/portfolio-create-react-app/index.html"
+  },
+  {
+    "revision": "aea8fb5c8cac18a74acb",
+    "url": "/portfolio-create-react-app/static/css/main.bdae5be4.chunk.css"
+  },
+  {
+    "revision": "d944eaa4dc406300a3d5",
+    "url": "/portfolio-create-react-app/static/js/2.09decf7a.chunk.js"
+  },
+  {
+    "revision": "aea8fb5c8cac18a74acb",
+    "url": "/portfolio-create-react-app/static/js/main.5d34e8e8.chunk.js"
+  },
+  {
+    "revision": "999046f5fad93a8fea10",
+    "url": "/portfolio-create-react-app/static/js/runtime-main.36396636.js"
+  }
+]);
